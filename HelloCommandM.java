a7770722897232ff4fd5091c89514c4d58ec6172
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-03-26 10:37:57 +0530

    added StringBuilder to print multiple inline arguments

--- HelloCommandM.java
+++ HelloCommandM.java
@@ -1,13 +1,19 @@
 public class HelloCommandM {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String name = "World";
-        if (args.length == 0){
-            System.out.println("Hello, "+name+ " !");
-        } else{
-            for(int i= 0; i<args.length; i++){
-                System.out.println("Hello, "+args[i]+"!");
-            }
-        }
+        StringBuilder nameBuilder = new StringBuilder();
 
-    } 
+        if (args.length > 0) {
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+            name = nameBuilder.toString();
+        }
+        System.out.println("Hello " + name);
+        
+    }
 }
