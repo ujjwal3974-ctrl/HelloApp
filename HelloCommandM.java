96da05d8864dc2f726d85e6b89e5f1e45767818e
ujjwal3974-ctrl <ujjwal3974@gmail.com>
2026-03-26 10:09:24 +0530

    Updated the code to print multiple line arguments

--- HelloCommandM.java
+++ HelloCommandM.java
@@ -2,12 +2,12 @@ public class HelloCommandM {
     public static void main(String[] args){
         String name = "World";
         if (args.length == 0){
-            System.out.println("Hello, "+name+ "!");
+            System.out.println("Hello, "+name+ " !");
         } else{
             for(int i= 0; i<args.length; i++){
                 System.out.println("Hello, "+args[i]+"!");
             }
         }
 
-    }
+    } 
 }
